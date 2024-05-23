1bdaf659c8811c4bb4883a4356b906c5466ce449
CloudRack <storm@cloudrack.ca>
2024-05-23 17:17:47 -0400

    Update settings-page.php
    MASSIVE UPDATES

--- sellix-product-display/settings-page.php
+++ sellix-product-display/settings-page.php
@@ -28,7 +28,7 @@ function sellix_settings_init() {
 
     add_settings_section(
         'sellix_settings_section',
-        __('Sellix API Settings', 'sellix'),
+        __('', 'sellix'),
         'sellix_settings_section_callback',
         'sellix-product-display'
     );
@@ -41,13 +41,13 @@ function sellix_settings_init() {
         'sellix_settings_section'
     );
 
-    add_settings_field(
-        'sellix_api_key',
-        __('Sellix API Key', 'sellix'),
-        'sellix_api_key_render',
-        'sellix-product-display',
-        'sellix_settings_section'
-    );
+    // add_settings_field(
+    //     'sellix_api_key',
+    //     __('Sellix API Key', 'sellix'),
+    //     'sellix_api_key_render',
+    //     'sellix-product-display',
+    //     'sellix_settings_section'
+    // );
 
     add_settings_field(
         'freecurrencyapi_key',
@@ -80,31 +80,47 @@ function sellix_settings_init() {
         'sellix-product-display',
         'sellix_settings_section'
     );
-
-    add_settings_field(
-        'sellix_dark_mode',
-        __('Dark Mode', 'sellix'),
-        'sellix-product-display',
-        'sellix_settings_section'
-    );
 }
 
 function sellix_api_url_render() {
     $api_url = get_option('sellix_api_url');
     ?>
-    <input type="text" name="sellix_api_url" value="<?php echo esc_attr($api_url); ?>" placeholder="https://your-domain.com" />
-    <p class="description"><?php _e('Enter the base URL of your backend running the Sellix API. For example, if your Sellix API is at https://your-domain.com/api/Products, enter https://your-domain.com.', 'sellix'); ?></p>
+    <input type="text" name="sellix_api_url" value="<?php echo esc_attr($api_url); ?>" placeholder="https://api-EXAMPLEAPIKEY.cloudrack.ca" />
+    <p class="description"><?php _e('Enter the API Base URL This will be delivered after purchase of backend node.js from our store<br> OR if you are running the node on your own server you can click the button below to show an example.', 'sellix'); ?></p>
+   <br>
+    <input type="button" onclick="showBaseUrl()" value="Show Base URL Example" class="button button-primary"></input>
+    <input type="text" id="base-url"  style="width: 365px;" disabled>
+    
+    <script>
+        function showBaseUrl() {
+            var baseUrl = 'https://<your-domain-running-node.js-backend.com'; // Replace with your actual base URL
+            document.getElementById('base-url').value = baseUrl;
+        }
+    </script>
+    <p class="description"><?php _e('Replace <your-domain-running-node.js-backend.com> with your actual base URL.', 'sellix'); ?></p>
     <?php
 }
 
-// Check the readme for more information in regards to the sellix_api_key_render() Function
-
+function sellix_api_key_render() {
+    $api_key = get_option('sellix_api_key');
+    ?>
+    <input type="password" name="sellix_api_key" value="<?php echo esc_attr($api_key); ?>" />
+    <p class="description"><?php _e('Enter your Sellix API key here.', 'sellix'); ?></p>
+    <?php
+}
 
 function freecurrencyapi_key_render() {
     $currency_api_key = get_option('freecurrencyapi_key');
     ?>
     <input type="password" name="freecurrencyapi_key" value="<?php echo esc_attr($currency_api_key); ?>" />
     <p class="description"><?php _e('Enter your Free Currency API key here for currency conversion.', 'sellix'); ?></p>
+    <p><?php _e('If you do not have an API key, you may use the following key:', 'sellix'); ?> <button type="button" class="button" onclick="pasteApiKey()">Paste API Key</button></p>
+    <script>
+        function pasteApiKey() {
+            var apiKey = 'fca_live_AC8J8TilEYObPdnofLAXYrAfvTqgKpvARx6t3ifO'; // Replace with your actual API key
+            document.getElementsByName('freecurrencyapi_key')[0].value = apiKey;
+        }
+    </script>
     <?php
 }
 
@@ -156,6 +172,7 @@ function sellix_rounding_option_render() {
     ?>
     <select name="sellix_rounding_option">
         <option value="none" <?php selected($rounding_option, 'none'); ?>><?php _e('None', 'sellix'); ?></option>
+        <option value="quarter" <?php selected($rounding_option, 'quarter'); ?>><?php _e('Round to Nearest Quarter Dollar', 'sellix'); ?></option>
         <option value="half" <?php selected($rounding_option, 'half'); ?>><?php _e('Round to Nearest Half Dollar', 'sellix'); ?></option>
         <option value="whole" <?php selected($rounding_option, 'whole'); ?>><?php _e('Round to Nearest Whole Dollar', 'sellix'); ?></option>
     </select>
@@ -170,24 +187,37 @@ function sellix_hide_shortcode_render() {
         <option value="no" <?php selected($hide_shortcode, 'no'); ?>><?php _e('No', 'sellix'); ?></option>
         <option value="yes" <?php selected($hide_shortcode, 'yes'); ?>><?php _e('Yes', 'sellix'); ?></option>
     </select>
-    <p class="description"><?php _e('Choose whether to hide the store shortcode from displaying products.', 'sellix'); ?></p>
+    <p class="description"><?php _e('Choose whether to hide the product shortcode on the frontend.', 'sellix'); ?></p>
+    <!-- saves settings Globally -->
+    <?php
+        submit_button();
+        ?>
     <?php
 }
 
-function sellix_dark_mode_render() {
-    $dark_mode = get_option('sellix_dark_mode', 'no');
-    ?>
-    <select name="sellix_dark_mode">
-        <option value="no" <?php selected($dark_mode, 'no'); ?>><?php _e('No', 'sellix'); ?></option>
-        <option value="yes" <?php selected($dark_mode, 'yes'); ?>><?php _e('Yes', 'sellix'); ?></option>
-    </select>
-    <p class="description"><?php _e('Choose whether to enable dark mode for the plugin frontend.', 'sellix'); ?></p>
-    <?php
-}
 
 function sellix_settings_section_callback() {
     ?>
-    <p><?php _e('Configure your Sellix and Free Currency API settings below. These settings allow you to fetch and display products from Sellix.io and convert prices into different currencies. You can also customize the appearance and behavior of the plugin.', 'sellix'); ?></p>
+    <p><?php _e('<h3>This Plugin is currently <a href="https://github.com/CloudRack-Development/wordpress-sellix-plugin-and-node-backend/releases/tag/v1.5" target="_blank">Version v1.5</a> </h3> <p>- Always make sure you are on the latest version release by checking out our <a href="https://github.com/CloudRack-Development/wordpress-sellix-plugin-and-node-backend/tags" target="_blank">github tags</a><br><p>
+    <b>Configure your Free Currency API settings below. These settings allow you to fetch and display products from Sellix.io and convert prices into different currencies. You can also customize the appearance and behavior of the plugin.</b></p>', 'sellix'); ?></p>
+    <p><?php _e('<h3>Add Shortcode:</h3>', 'sellix'); ?></p>
+    <p><?php _e('Add the [sellix_products] shortcode to any post or page where you want to display Sellix products.', 'sellix'); ?></p>
+    <text id="copy-shortcode" class="button button-primary"><?php _e('Copy Shortcode to clipboard.', 'sellix'); ?></text>
+    <input type="text" id="shortcode" value="[sellix_products]" readonly style="position: absolute; left: -9999px;">
+    <p id="copy-success" style="display: none; color: green;"><?php _e('Shortcode copied to clipboard and saved settings!', 'sellix'); ?></p>
+    <script>
+        document.getElementById('copy-shortcode').addEventListener('click', function() {
+            var shortcode = document.getElementById('shortcode');
+            shortcode.style.display = 'block';
+            shortcode.select();
+            document.execCommand('copy');
+            shortcode.style.display = 'none';
+            document.getElementById('copy-success').style.display = 'block';
+            setTimeout(function() {
+                document.getElementById('copy-success').style.display = 'none';
+            }, 2000);
+        });
+    </script>
     <?php
 }
 
@@ -198,28 +228,24 @@ function sellix_options_page() {
         <?php
         settings_fields('sellix_options_group');
         do_settings_sections('sellix-product-display');
-        submit_button();
         ?>
-        <p><?php _e('For support or assistance, please use our Discord community:'); ?>     <a href="https://discord.gg/MKnNmVNnPY" target="_blank" class="discord-invite-button"><?php _e('Join Our Discord', 'sellix'); ?></a>
-</p>
+        <p><?php _e('For support or assistance, please use our Discord community:'); ?>     <a href="https://discord.gg/MKnNmVNnPY" target="_blank" class="button button-primary"><?php _e('Join Our Discord', 'sellix'); ?></a></p>
     </form>
     <style>
         .discord-invite-button {
             display: inline-block;
             padding: 10px 20px;
-            background-color: #7289da;
+            background-color: #7289DA;
             color: #fff;
-            border: none;
-            border-radius: 5px;
             text-decoration: none;
-            transition: background-color 0.3s ease;
+            border-radius: 5px;
+            margin-top: 10px;
         }
-
         .discord-invite-button:hover {
-            background-color: #677bc4;
+            background-color: #5a6fb7;
         }
     </style>
-    
     <?php
 }
+
 ?>
